strawpay/bitcoinj

When deserializing wallet extensions, lock order is now wallet and then extension.

core/src/main/java/org/bitcoinj/store/WalletProtobufSerializer.java
@@ -513,13 +513,15 @@ public class WalletProtobufSerializer {
             } else {
                 log.info("Loading wallet extension {}", id);
                 try {
-                    extension.deserializeWalletExtension(wallet, extProto.getData().toByteArray());
+                    wallet.deserializeWalletExtension(extension, extProto.getData().toByteArray());
                     wallet.addOrGetExistingExtension(extension);
                 } catch (Exception e) {
-                    if (extProto.getMandatory() && requireMandatoryExtensions)
+                    if (extProto.getMandatory() && requireMandatoryExtensions) {
+                        log.error("Error whilst reading extension {}, failing to read wallet", id, e);
                         throw new UnreadableWalletException("Could not parse mandatory extension in wallet: " + id);
-                    else
+                    } else {
                         log.error("Error whilst reading extension {}, ignoring", id, e);
+                    }
                 }
             }
         }

wagerr/wagerrj

core/src/main/java/org/bitcoinj/store/WalletProtobufSerializer.java
@@ -513,13 +513,14 @@ public class WalletProtobufSerializer {
             } else {
                 log.info("Loading wallet extension {}", id);
                 try {
-                    extension.deserializeWalletExtension(wallet, extProto.getData().toByteArray());
-                    wallet.addOrGetExistingExtension(extension);
+                    wallet.deserializeAndAddExtension(extension, extProto.getData().toByteArray());
                 } catch (Exception e) {
-                    if (extProto.getMandatory() && requireMandatoryExtensions)
+                    if (extProto.getMandatory() && requireMandatoryExtensions) {
+                        log.error("Error whilst reading extension {}, failing to read wallet", id, e);
                         throw new UnreadableWalletException("Could not parse mandatory extension in wallet: " + id);
-                    else
+                    } else {
                         log.error("Error whilst reading extension {}, ignoring", id, e);
+                    }
                 }
             }
         }